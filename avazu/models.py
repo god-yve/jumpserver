2d0c42d1ba9afa4e88b58ae3f4ae478bd87f4270
root <root@localhost.localdomain>
2016-10-08 11:47:06 +0800

    用户保存注册用户信息的model

--- avazu/models.py
+++ avazu/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class RegisterUser(models.Model):
+    username = models.CharField(max_length=50)
+    name =  models.CharField(max_length=50)
+    password = models.CharField(max_length=30)
+    hosts = models.CharField(max_length=500)
+    email = models.EmailField(max_length=50)
+    expire_date = models.DateTimeField()
+    is_added = models.SmallIntegerField(default=0)
+  
+    def __unicode__(self):
+        return '%s(%s):%s' % (self.username, self.name,  self.email)
+
