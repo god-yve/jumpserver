660158572767086d641debb20e7bc6fb20bef4ee
root <root@localhost.localdomain>
2016-10-17 14:50:07 +0800

    增加有效期字段 增加用户注册时间字段

--- avazu/models.py
+++ avazu/models.py
@@ -6,10 +6,9 @@ class RegisterUser(models.Model):
     username = models.CharField(max_length=50)
     name =  models.CharField(max_length=50)
     password = models.CharField(max_length=30)
-    expire = models.PositiveSmallIntegerField()
     email = models.EmailField(max_length=50)
-    expire_date = models.DateTimeField()
-    #registered_date = models.DateTimeField(auto_now_add=True)
+    expire = models.PositiveSmallIntegerField()
+    register_date = models.DateTimeField(auto_now_add=True)
     is_added = models.SmallIntegerField(default=0)
   
     def __unicode__(self):
